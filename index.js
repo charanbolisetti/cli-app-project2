5e0a25d90739624b28728db31d40778bab1860c5
Bolisetti Durga Charan Naidu <charannbolisetti@gmail.com>
2020-12-01 22:46:43 +0530

    validating answer

--- index.js
+++ index.js
@@ -18,3 +18,24 @@ console.log(chalk.bgBlueBright("HULK"));
 console.log(chalk.bgMagenta("BLACK WIDOW"));
 console.log(chalk.bgYellow("BLACK PANTHER"));
 console.log("-------------------------------------------");
+
+
+
+
+function load(question,answer){
+    console.log(currentQuestion.question);
+    console.log("a)" + currentQuestion.a);
+    console.log("b)" + currentQuestion.b);
+    console.log("c)" + currentQuestion.c);
+    console.log("d)" + currentQuestion.d);
+    var userAnswer=readlineSync.question("Enter your option: ");
+    if(userAnswer === answer){
+      console.log(chalk.cyanBright("You are Right?"));
+      score=score+1;
+    }else{
+      console.log(chalk.cyanBright("You are Wrong"));
+      score=score-1;
+    }
+    console.log(chalk.yellowBright("Your score is " + score));
+    console.log("---------------");
+  }
